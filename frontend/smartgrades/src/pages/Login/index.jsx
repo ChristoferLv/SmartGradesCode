835440ed4452c392b5c70a08c10d3fc8403d3ff7
ChristoferLv <christofer_daniel12@hotmail.com>
2024-10-08 15:02:36 -0300

    Feitos ajustes no login

--- frontend/smartgrades/src/pages/Login/index.jsx
+++ frontend/smartgrades/src/pages/Login/index.jsx
@@ -40,9 +40,7 @@ const LoginPage = () => {
   }
 
   const saveUserDataOnLocalStorage = () => {
-    console.log('LoginPage::saveUserDataOnLocalStorage(): ')
     const jwt = localStorage.getItem('token')
-    console.log('LoginPage::saveUserDataOnLocalStorage(): ', jwt)
     AuthAPI.getUserInfo(jwt).then((response) => {
       if (response.status === HttpStatus.OK) {
         localStorage.setItem('userData', JSON.stringify(response.data))
@@ -80,8 +78,8 @@ const LoginPage = () => {
         }
         setIsLoading(false)
       } else {
-        console.log(responseLogin)
-        //notifyError('Falha ao executar login. ' + responseLogin.data.error)
+        //console.log(responseLogin)
+        notifyError('Falha ao executar login. ' + responseLogin.data)
       }
       setIsLoading(false)
     }
